a0defa8f8989ebaa4d1d509170ceba7665cf13ee
github username <github email address>
2020-06-27 20:02:18 -0400

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
-class StockContainer extends Component {
+const StockContainer = props =>  {
+    let stocks = props.stocks
 
-  render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {stocks.map(stock => <Stock key={stock.id} stock={stock} stockAction={props.addPortfolio} /> )}
       </div>
     );
-  }
-
 }
 
 export default StockContainer;
