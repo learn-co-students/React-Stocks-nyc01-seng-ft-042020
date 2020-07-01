709f550cf65642e94df1f452b5368d2b4232bbc2
dnamgyal <don.namgyal@hotmail.com>
2020-06-30 23:08:26 -0400

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,20 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+
+    //console.log(this.props)
+    const stocksList = () => {
+      let stocksToReturn = this.props.stocks.map(stock => <Stock stock={stock} key={stock.id} action={this.props.action}/>)
+      return stocksToReturn
+    }
+
+
+
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          stocksList()
         }
       </div>
     );
